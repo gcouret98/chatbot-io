dbe91b68eef5e62faa74d6a25928175661db6e86
Meewk <g.couret@intech-sud.fr>
2023-01-19 18:04:13 +0100

    todo réponse avec bot class

--- src/bot-class.js
+++ src/bot-class.js
@@ -1,9 +1,12 @@
 const Bot = class Bot {
-    constructor(name, urlPicture, newMessages) {
+    constructor(id, name, urlPicture, newMessages, actions) {
+        this.id = id;
         this.name = name;
         this.urlPicture = urlPicture;
         this.newMessages = newMessages;
+        this.actions = actions;
     }
+
 };
 
 export default Bot;
